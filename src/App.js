3dffe312875a995036258d45f7389d5d601aaa2a
Quinn94 <mayquinn94@gmail.com>
2020-04-28 23:24:55 +0800

    lazy loading, utility

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
-import React, { Component } from 'react';
+import React, { Component} from 'react';
 import {Route, Switch, Redirect} from 'react-router-dom';
 import {connect} from 'react-redux';
 
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
-import Checkout from './containers/Checkout/Checkout';
-import Orders from './containers/Orders/Orders';
-import Auth from './containers/Auth/Auth';
+// import Checkout from './containers/Checkout/Checkout';
+// import Orders from './containers/Orders/Orders';
+// import Auth from './containers/Auth/Auth';
 import Logout from './containers/Logout/Logout';
 import * as actions from './store/actions/index';
+import asyncComponent from './hoc/asyncComponent/asyncComponent';
+
+const asyncCheckout = asyncComponent ( () => {
+  return import('./containers/Checkout/Checkout')
+});
+
+const asyncOrders = asyncComponent ( () => {
+  return import('./containers/Orders/Orders')
+});
+
+const asyncAuth = asyncComponent ( () => {
+  return import('./containers/Auth/Auth')
+});
+//lazy loading chuck1,2,3
 
 class App extends Component {
   componentDidMount(){
@@ -17,7 +31,7 @@ class App extends Component {
   render () {
     let route = (
       <Switch>
-        <Route path="/auth" component={Auth} />
+        <Route path="/auth" component={asyncAuth} />
         <Route path="/" component={BurgerBuilder} />
         <Redirect to='/' />
       </Switch>
@@ -26,16 +40,16 @@ class App extends Component {
     if(this.props.isAuthenticated){
       route = (
         <Switch>
-          <Route path="/checkout" component={Checkout} />
-          <Route path="/orders" component={Orders} />
+          <Route path="/checkout" component={asyncCheckout} />
+          <Route path="/orders" component={asyncOrders} />
           <Route path="/logout" component={Logout} />
-          <Route path="/auth" component={Auth} />
+          <Route path="/auth" component={asyncAuth} />
           <Route path="/" component={BurgerBuilder} />
           <Redirect to='/' />
         </Switch>
       )
     }
-    //now we cannot reach orders page is we are not logged in (guarded route)
+    //now we cannot reach orders page if we are not logged in (guarded route)
     return (
       <div>
         <Layout>
